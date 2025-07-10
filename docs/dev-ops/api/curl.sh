3c251e0d1313275f0591c01eb9659f1e6d1c3867
luojiawei <2240774934@qq.com>
2025-07-10 23:46:37 +0800

    feat： 2-init

---
+++ docs/dev-ops/api/curl.sh
@@ -0,0 +1,7 @@
+curl http://81.71.3.20:11434/api/generate \
+  -H "Content-Type: application/json" \
+  -d '{
+        "model": "deepseek-r1:1.5b",
+        "prompt": "1+1",
+        "stream": false
+      }'
